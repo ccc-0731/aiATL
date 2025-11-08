4472958f7cf963bc1df78dc75d6e739d6ddc7781
mayflower99 <104937613+mayflower99@users.noreply.github.com>
2025-11-08 00:29:35 -0500

    finished controller probably broken

--- Christian/controller.py
+++ Christian/controller.py
@@ -11,34 +11,48 @@ app = Flask(__name__)
 
 app.secret_key = "hi"
 
+#takes in a filename and checks if the extention is allowed
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#renders the index page
+#if not logged in sends user to login
 @app.route('/')
 def index():  # put application's code here
     if "logged_in" not in session:
+        #redirects to login
         return render_template('login_form.html')
+    #adds username to the session cookie which just acts as a dictionary
     username = session["username"]
+    #renders index
     return render_template('index.html')
 
-
+#renders a login form to get a username
 @app.route('/login_form', methods=['POST'])
 def login():
+    #if your logged in redirect to index
     if "logged_in" in session:
         return render_template('index.html')
+    #stores username in the json file if it's not there and in the session cookie
     username = request.form.get('username')
     session['username'] = username
+    if (username not in DAL.get_users_as_list):
+        DAL.add_new_user(username)
+    #adds a cookie to say you're logged in
     session["logged_in"] = True
+    #sends you to index
     return redirect(url_for('index'))
 
-
+#logs you out
 @app.route('/logout')
 def logout():
+    #removes all info from cookie
     session.pop("username", None)
     session.pop("logged_in", None)
     return redirect(url_for('index'))
 
+#uploads the file to 
 @app.route("/upload", methods=['GET', 'POST'])
 def uploadFile():
     FORMNAME = 'file'
@@ -52,8 +66,11 @@ def uploadFile():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        #
         if file and allowed_file(file.filename):
+            #makes the filename safe so no malware
             filename = secure_filename(file.filename)
+            #saves the file to the images folder in database
             DAL.saveFile(file, session["username"])
             return redirect(url_for('download_file', name=filename))
     return
--- Christian/model/DAL.py
+++ Christian/model/DAL.py
@@ -5,15 +5,53 @@ UPLOAD_FOLDER = '~/database/images'
 def saveFile(file, username:str) -> None:
     file.save(os.path.join(UPLOAD_FOLDER, username))
 
+
+def set_sb() -> None:
+    """
+    sets up the database
+    :return: None
+    """
+    init_db = {
+        "jane1": {
+            "tasks": ["task1", "task2", "task3"],
+        }
+    }
+
+    f = open("database/task_list_DB.json", "w")
+    json.dump(init_db, f)
+    f.close()
+
+
 def get_db_as_dict() -> dict:
     """
     gets the database as a dictionary
     :return: the database as a dictionary
     """
-    f = open("database/task_list_DB.json", "r")
+    f = open("database/userdata.json", "r")
     data = json.load(f)
     f.close()
     return data
+
+
+def write_to_db(data: dict) -> None:
+    """
+    writes the data to the database
+    :param data: dictionary to write
+    :return: None
+    """
+    f = open("database/task_list_DB.json", "w")
+    json.dump(data, f)
+    f.close()
+
+def get_users_as_list() -> list:
+    db = get_db_as_dict()
+    return list(db)
+
+
+def add_new_user(username: str) -> None:
+    db = get_db_as_dict()
+    db[username] = {}
+    write_to_db(db)
 
 def get_users_as_list() -> list:
     db = get_db_as_dict()
